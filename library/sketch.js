33778b9df4eda51d60af3e0bebb6c332540d3031
xdKya <90745676+xdKya@users.noreply.github.com>
2022-07-08 15:11:42 -0300

    Add files via upload

---
+++ library/sketch.js
@@ -0,0 +1,34 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+
+function preload(){
+ 
+}
+
+function setup(){
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+ 
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+
+  
+}
+
+function draw(){
+  background(51);
+
+  Engine.update(engine);
+ 
+  drawSprites(); 
+}
